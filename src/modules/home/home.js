8173cd463fe0d119af97715bdac94caa72238d79
maksyuki <maksyuki@126.com>
2021-05-24 22:01:51 +0800

    feat: modify git ignore rules and register new cmd

--- src/modules/home/home.js
+++ src/modules/home/home.js
@@ -1,18 +1,19 @@
-const vscode   = require("vscode");
+const vscode = require("vscode");
 const utilExtn = require("../../utils/util_extn");
 
 // import vscode from "vscode";
 // import * as utilExtn from "../../utils/util_extn";
 
-module.exports = function(context) {
+module.exports = function (context) {
     context.subscriptions.push(
-        vscode.commands.registerCommand("treecore.cmd.showHomePage", function() {
+        vscode.commands.registerCommand("treecore.cmd.showHomePage", function () {
             const panel = vscode.window.createWebviewPanel(
                 "treecoreHome",
                 "TreeCore IDE Home",
                 vscode.ViewColumn.One,
                 {
-                    enableScripts: true
+                    enableScripts: true,
+                    retainContextWhenHidden: true,
                 }
             );
             let global = { panel };
@@ -32,6 +33,25 @@ module.exports = function(context) {
     if (vscode.workspace.getConfiguration().get(key)) {
         vscode.commands.executeCommand("treecore.cmd.showHomePage");
     }
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('waveviewer.start', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'Wave Viewer',
+                "Wave Viewer",
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    retainContextWhenHidden: true,
+                }
+            );
+
+            panel.webview.html = utilExtn.getWebViewContent(context,
+                'src/modules/viewer/waveform/WaveViewer.html');
+        }
+    ));
+
+
 }
 
 // export default function showHomePage(context) {
