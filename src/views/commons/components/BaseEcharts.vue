46573553456bd9ed03d8762b771385444b187bb4
myyerrol <myyerrol@126.com>
2021-07-22 22:44:59 +0800

    chore: add jquery package

--- src/views/commons/components/BaseEcharts.vue
+++ src/views/commons/components/BaseEcharts.vue
@@ -4,6 +4,7 @@
 <script>
 import Vue from "vue";
 import * as echarts from "echarts";
+import $ from "jquery";
 
 Vue.prototype.$echarts = echarts;
 
@@ -25,13 +26,54 @@ export default {
                     subtext: "全部模块",
                     left: "center"
                 },
+                legend: {
+                    left: "left",
+                    orient: "vertical"
+                },
                 tooltip: {
                     trigger: "item",
                     formatter: "{a} <br/>{b}: {c} ({d}%)"
                 },
-                legend: {
-                    orient: "vertical",
-                    left: "left",
+                toolbox: {
+                    feature: {
+                        dataView: {
+                            readOnly: false,
+                            optionToContent: function(opt) {
+                                // 关闭图表定时器
+                                clearInterval(chartTimer);
+                                // 渲染自定义表格
+                                console.log(opt);
+                                let series = opt.series;
+                                let seriesName = series[0].name;
+                                let seriesData = series[0].data;
+                                let table = "<table border='1' cellspacing='0' " +
+                                                   "style='width:100%;text-align:center;" +
+                                                   "color:black;'><tbody><tr>" +
+                                                "<td>指标类别</td>" +
+                                                "<td>指标内容</td>" +
+                                                "</tr>";
+                                for (let i = 0; i < seriesData.length; i++) {
+                                    table += "<tr>" +
+                                                "<td>" + seriesData[i].name + "</td>" +
+                                                "<td>" + seriesData[i].value + "</td>" +
+                                             "</tr>";
+                                }
+                                table += "</tbody></table>";
+                                return table;
+                            },
+                            contentToOption: function(dom, opt) {
+                                console.log(dom, opt);
+                                chartTimer = setInterval(chartTimerFunc, 3000);
+                            },
+                            lang: ["<div style='text-align:center;'>数据视图</div>",
+                                   "<div style='display:none;'>测试</div>",
+                                   '关闭']
+                        },
+                        saveAsImage: {
+                        },
+                        restore: {
+                        }
+                    }
                 },
                 series: [{
                     name: "占比情况",
@@ -61,10 +103,13 @@ export default {
                 backgroundColor: "rgb(30, 30, 30)"
             }
             chart.setOption(options);
+
+
             let currentIndex = -1;
-            setInterval(function() {
-                var dataLen = options.series[0].data.length;
-                // 取消之前高亮的图形
+            let chartTimer = 0;
+            function chartTimerFunc() {
+                let dataLen = options.series[0].data.length;
+                // 取消高亮图形
                 chart.dispatchAction({
                     type: "downplay",
                     seriesIndex: 0,
@@ -83,8 +128,33 @@ export default {
                     seriesIndex: 0,
                     dataIndex: currentIndex
                 });
-            }, 3000);
+            }
+            chartTimer = setInterval(chartTimerFunc, 3000);
+            // chartTimer = setInterval(function() {
+            //     let dataLen = options.series[0].data.length;
+            //     // 取消高亮图形
+            //     chart.dispatchAction({
+            //         type: "downplay",
+            //         seriesIndex: 0,
+            //         dataIndex: currentIndex
+            //     });
+            //     currentIndex = (currentIndex + 1) % dataLen;
+            //     // 高亮当前图形
+            //     chart.dispatchAction({
+            //         type: "highlight",
+            //         seriesIndex: 0,
+            //         dataIndex: currentIndex
+            //     });
+            //     // 显示tooltip
+            //     chart.dispatchAction({
+            //         type: "showTip",
+            //         seriesIndex: 0,
+            //         dataIndex: currentIndex
+            //     });
+            // }, 3000);
         }
     }
 }
 </script>
+<style scoped>
+</style>
