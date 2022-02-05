35cdcafd4a0f9a86d2baf924389ef16a64effdb9
myyerrol <myyerrol@126.com>
2022-02-05 18:04:25 +0800

    style: comment some unused codes

--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
 const vscode = require("vscode");
+const home = require("./native/modules/home/home");
 const ProjectManager = require("./native/modules/manager/ProjectManagerProvider");
-// import vscode from "vscode";
-// import showHomePage from "./native/home/home"
 
+// 插件被激活时触发
 // Triggered when the extension is activated
 function activate(context) {
     console.log("TreeCore IDE extension is active!");
     console.log(vscode);
 
-    require("./native/modules/home/home")(context);
+    home.showHomePage(context);
 
-    console.log(vscode.workspace.workspaceFolders)
-    const projectManagerProvider = new ProjectManager.ProjectManagerProvider(vscode.workspace.rootPath);
-    vscode.window.registerTreeDataProvider('treecore.activityBar.rtlproject', projectManagerProvider);
-    vscode.commands.registerCommand('treecore.activityBar.rtlproject.refreshEntry', () => projectManagerProvider.refresh());
+    // console.log(vscode.workspace.workspaceFolders)
+    // const projectManagerProvider = new ProjectManager.ProjectManagerProvider(vscode.workspace.rootPath);
+    // vscode.window.registerTreeDataProvider('treecore.activityBar.rtlproject', projectManagerProvider);
+    // vscode.commands.registerCommand('treecore.activityBar.rtlproject.refreshEntry', () => projectManagerProvider.refresh());
 
-    // Need to handle the bug when in multi-workspace condition
-    vscode.commands.registerCommand('extension.openPackageOnNpm', filePath => vscode.commands.executeCommand('vscode.open', vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, `${filePath}`)));
-    vscode.commands.registerCommand('treecore.activityBar.rtlproject.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-    vscode.commands.registerCommand('treecore.activityBar.rtlproject.editEntry', (node) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-    vscode.commands.registerCommand('treecore.activityBar.rtlproject.deleteEntry', (node) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+    // // Need to handle the bug when in multi-workspace condition
+    // vscode.commands.registerCommand('extension.openPackageOnNpm', filePath => vscode.commands.executeCommand('vscode.open', vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, `${filePath}`)));
+    // vscode.commands.registerCommand('treecore.activityBar.rtlproject.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+    // vscode.commands.registerCommand('treecore.activityBar.rtlproject.editEntry', (node) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+    // vscode.commands.registerCommand('treecore.activityBar.rtlproject.deleteEntry', (node) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
 }
 
+// 插件被释放时触发
 // Triggered when the extension is deactivated
 function deactivate() {
     console.log("TreeCore IDE extension is deactive!");
@@ -31,18 +32,3 @@ module.exports = {
     activate,
     deactivate
 }
-
-// // 插件被激活时触发
-// // Triggered when the extension is activated
-// export function activate(context) {
-//     console.log("TreeCore IDE extension is active!");
-//     console.log(vscode);
-
-//     showHomePage(context);
-// }
-
-// // 插件被释放时触发
-// // Triggered when the extension is deactivated
-// export function deactivate() {
-//     console.log("TreeCore IDE extension is deactive!");
-// }
--- src/native/modules/home/home.js
+++ src/native/modules/home/home.js
@@ -1,67 +1,101 @@
 const vscode = require("vscode");
-const utilExtn = require("../../utils/extn");
+const extn = require("../../utils/extn");
 
-module.exports = function(context) {
-    context.subscriptions.push(
-        vscode.commands.registerCommand("treecore.cmd.showHomePage", () => {
-            const panel = vscode.window.createWebviewPanel(
-                "treecoreHome",
-                "TreeCore IDE Home",
-                vscode.ViewColumn.One,
-                {
-                    enableScripts: true,
-                    retainContextWhenHidden: true,
-                }
-            );
-            let global = { panel };
-            panel.iconPath = utilExtn.getExtensionFileAbsolutePathUrl(
-                context,
-                "resources/images/logos/treecore_logo_main.svg");
-            panel.webview.html = utilExtn.getWebViewContent(
-                context,
-                "src/server/static/index.html");
-            panel.webview.onDidReceiveMessage((message) => {
-                utilExtn.handleMessageFromWebview(global, message);
-            }, undefined, context.subscriptions);
+module.exports = {
+    showHomePage: function(context) {
+        context.subscriptions.push(
+            vscode.commands.registerCommand("treecore.cmd.showHomePage", () => {
+                const panel = vscode.window.createWebviewPanel(
+                    "treecoreHome",
+                    "TreeCore IDE Home",
+                    vscode.ViewColumn.One,
+                    {
+                        enableScripts: true,
+                        retainContextWhenHidden: true,
+                    }
+                );
+                let global = { panel };
+                panel.iconPath = extn.getExtensionFileAbsolutePathUrl(
+                    context,
+                    "resources/images/logos/treecore_logo_main.svg");
+                panel.webview.html = extn.getWebViewContent(
+                    context,
+                    "src/server/static/index.html");
+                panel.webview.onDidReceiveMessage((message) => {
+                    extn.handleMessageFromWebview(global, message);
+                },
+                undefined,
+                context.subscriptions);
+            }
+        ));
+        const key = "treecore.config.showHomePageAtStartup";
+        if (vscode.workspace.getConfiguration().get(key)) {
+            vscode.commands.executeCommand("treecore.cmd.showHomePage");
         }
-    ));
-
-    const key = "treecore.config.showHomePageAtStartup";
-    if (vscode.workspace.getConfiguration().get(key)) {
-        vscode.commands.executeCommand("treecore.cmd.showHomePage");
     }
+};
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("waveviewer.start", () => {
-            const panel = vscode.window.createWebviewPanel(
-                "Wave Viewer",
-                "Wave Viewer",
-                vscode.ViewColumn.One,
-                {
-                    enableScripts: true,
-                    retainContextWhenHidden: true,
-                }
-            );
+// module.exports = function(context) {
+//     context.subscriptions.push(
+//         vscode.commands.registerCommand("treecore.cmd.showHomePage", () => {
+//             const panel = vscode.window.createWebviewPanel(
+//                 "treecoreHome",
+//                 "TreeCore IDE Home",
+//                 vscode.ViewColumn.One,
+//                 {
+//                     enableScripts: true,
+//                     retainContextWhenHidden: true,
+//                 }
+//             );
+//             let global = { panel };
+//             panel.iconPath = extn.getExtensionFileAbsolutePathUrl(
+//                 context,
+//                 "resources/images/logos/treecore_logo_main.svg");
+//             panel.webview.html = extn.getWebViewContent(
+//                 context,
+//                 "src/server/static/index.html");
+//             panel.webview.onDidReceiveMessage((message) => {
+//                 extn.handleMessageFromWebview(global, message);
+//             }, undefined, context.subscriptions);
+//         }
+//     ));
 
-            panel.webview.html = utilExtn.getWebViewContent(context,
-                "src/native/viewer/waveform/WaveViewer.html");
-        }
-    ));
+//     const key = "treecore.config.showHomePageAtStartup";
+//     if (vscode.workspace.getConfiguration().get(key)) {
+//         vscode.commands.executeCommand("treecore.cmd.showHomePage");
+//     }
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand("schviewer.start", () => {
-            const panel = vscode.window.createWebviewPanel(
-                "Sch Viewer",
-                "Sch Viewer",
-                vscode.ViewColumn.One,
-                {
-                    enableScripts: true,
-                    retainContextWhenHidden: true,
-                }
-            );
+    // context.subscriptions.push(
+    //     vscode.commands.registerCommand("waveviewer.start", () => {
+    //         const panel = vscode.window.createWebviewPanel(
+    //             "Wave Viewer",
+    //             "Wave Viewer",
+    //             vscode.ViewColumn.One,
+    //             {
+    //                 enableScripts: true,
+    //                 retainContextWhenHidden: true,
+    //             }
+    //         );
 
-            panel.webview.html = utilExtn.getWebViewContent(context,
-                "src/native/viewer/schematic/SchViewer.html");
-        }
-    ));
-}
+    //         panel.webview.html = extn.getWebViewContent(context,
+    //             "src/native/viewer/waveform/WaveViewer.html");
+    //     }
+    // ));
+
+    // context.subscriptions.push(
+    //     vscode.commands.registerCommand("schviewer.start", () => {
+    //         const panel = vscode.window.createWebviewPanel(
+    //             "Sch Viewer",
+    //             "Sch Viewer",
+    //             vscode.ViewColumn.One,
+    //             {
+    //                 enableScripts: true,
+    //                 retainContextWhenHidden: true,
+    //             }
+    //         );
+
+    //         panel.webview.html = extn.getWebViewContent(context,
+    //             "src/native/viewer/schematic/SchViewer.html");
+    //     }
+    // ));
+// }
