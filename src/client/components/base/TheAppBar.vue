cb1fc43c836d094ea9c18cf01f0a52378da67aa8
myyerrol <myyerrol@126.com>
2022-02-10 09:18:48 +0800

    style: add a enter line

--- src/client/components/base/TheAppBar.vue
+++ src/client/components/base/TheAppBar.vue
@@ -1,6 +1,7 @@
 <template>
-    <v-app-bar app
-               height="60px">
+    <v-app-bar
+        app
+        height="60px">
         <div v-for="item in appBarItems"
              :key="item.title">
             <v-btn
